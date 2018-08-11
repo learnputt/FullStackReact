8cee00e762258d48e80ba0196de2e51ba6a2305b
Sree <sreelatha.putturi@gmail.com>
2018-08-11 18:24:02 -0500

    after stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'crazy dance' });
+  res.send({ hi: 'After crazy dance & changes for S3 ...' });
 });
 
 const PORT = process.env.PORT || 5000;
