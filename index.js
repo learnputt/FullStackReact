72673c5befbe38331239b7815ec323b10958d6f3
Sree <sreelatha.putturi@gmail.com>
2018-08-11 17:15:51 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'you there' });
+  res.send({ hi: 'changed after first deploy there' });
 });
 
 const PORT = process.env.PORT || 5000;
